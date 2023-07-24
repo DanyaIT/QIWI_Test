9b447d40fbc88bf5f550386db9c80b9816ee534f
DanyaIT <105100908+DanyaIT@users.noreply.github.com>
2023-07-24 16:27:54 +0300

    commit

--- app.js
+++ app.js
@@ -1,72 +1,65 @@
 
+const options = document.getElementById('currency-selector')
+const currencyInfo = document.getElementById('currency-info');
 
-document.getElementById('currency-selector').addEventListener('change', function () {
-    
-    const selectedCurrency = this.value;
-    fetchData(selectedCurrency)
-});
-    
-const fetchData = async (selectedCurrency) => {
-    const responce = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-    const data = await responce.json()
-    printData(data, selectedCurrency)
-}
-const printData = (data, selectedCurrency) => {
-    const dataAboutValute = data.Valute[selectedCurrency];
- 
-    const currentData = {
-        id: dataAboutValute.ID,
-        name: dataAboutValute.Name,
-        code: dataAboutValute.CharCode,
-        prevValue: dataAboutValute.Previous,
-        numCode: dataAboutValute.NumCode
-    };
+window.addEventListener('DOMContentLoaded', () => {
+    renderData()
 
-    const currentTime = {
-        date: data.Date,
-        prevDate: data.PreviousDate
+})
+
+options.addEventListener('change', async (e) => {
+    try {
+        const data = await fetchData()
+        const currentValue = e.target.value
+        const dataOfValute = data.Valute[currentValue]
+        
+        currencyInfo.innerHTML = `
+        <p><strong>ID валюты:</strong> ${dataOfValute.ID}</p>
+        <p><strong>Название валюты:</strong> ${dataOfValute.Name}</p>
+        <p><strong>Код валюты:</strong> ${dataOfValute.CharCode}</p>
+        <p><strong>Дата - настояшие:</strong> ${data.Date}
+        <p><strong>Дата - прошлое:</strong> ${data.PreviousDate}`;
+    
+        options.insertAdjacentElement = `
+            <option value="${e.target}">${currentValue}</option>`;
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+const renderData = async () => {
+    try {
+        const data = await fetchData()
+        Object.entries(data.Valute).forEach(([key, value]) => {
+            options.innerHTML += `
+            <option value="${key}">${value.Name}</option>`
+    
+        })
+        
+    } catch (error) {
+        console.log(error)
     }
 
 
 
-    const currencyOptions = document.getElementById('currency-selector')
-    currencyOptions.innerHTML = Object.entries(data.Valute).map(item => 
-        `<option value="${item[selectedCurrency]}">${item[0]} - ${item[1].Name}</option>`
-     
-    )
-    
-    
-    
-    const currencyInfo = document.getElementById('currency-info');
-    currencyInfo.innerHTML = `
-    <p><strong>ID валюты:</strong> ${currentData.id}</p>
-    <p><strong>Название валюты:</strong> ${currentData.name}</p>
-    <p><strong>Код валюты:</strong> ${currentData.code}</p>
-    <p><strong>Дата - значение:</strong> ${currentTime.date}</p>
-    <p><strong>Предшествующая дата - предшествующее значение:</strong> ${currentTime.prevDate}</p>`
-    
+}
+
+const fetchData = async () => {
+    try {
+        const responce = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+        const data = await responce.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 
 
-// Обработчик события при выборе валюты
 
-         // let array = [currentData].map(item => {
-         //     return {
-         //         ...item,     
-         //     }
-         // });
- 
-         // if(localStorage.getItem('dataValute') == null){
-         //     var myArray =[];
-         // }else{
-         //     myArray =  JSON.parse(localStorage.getItem('dataValute'));
- 
-         // }
-     
-         // function addItemToArray(){
-         //     myArray.push(currentData);
-         //     localStorage.setItem('dataValute', JSON.stringify(myArray));
-          
-         // }
-         // addItemToArray()
+
+
+
